0ad883a1c77975ed902347f91a42d9467c269965
Casper Kizewski <163021@student.horizoncollege.nl>
2022-06-14 09:54:58 +0200

    Hallo

--- Python_Files/index.py
+++ Python_Files/index.py
@@ -1,27 +1,29 @@
-import pandas
-import mysql.connector
+import pandas as pd
+import pyodbc
+# import mysql.connector
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="Toverlantaarn"
-)
+# mydb = mysql.connector.connect(
+#     host="localhost",
+#     user="root",
+#     password="",
+#     database="Toverlantaarn"
+# )
 
-mycursor = mydb.cursor()
+server = 'localhost' 
+database = 'Toverlantaarn' 
+username = 'root' 
+password = '' 
+cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
 
-df = pandas.read_csv('Museum-collectie-beschrijving.csv')
+df = pd.read_csv("Museum-collectie-beschrijvinging.csv")
 
-# print(df)
+print(df)
 
-# for item in range(0, 44):
-#     # print(df[item])
-#     sql = "INSERT INTO lantaarn (Objectcode) VALUES (%s)"
-#     val = (df['Objectcode'][4])
-#     print(val)
-#     mycursor.execute(sql, val)
-#     mydb.commit()
-#     print(mycursor.rowcount, "record inserted.")
+for index, row in df.iterrows():
+    cursor.execute("INSERT INTO Toverlantaarn.lantaarn (Objectcode, Object, Categorie, Type, Unnamed, Afmeting, Beschrijving, Operationeel, Conditie, Foto) values(?,?,?,?,?,?,?,?,?,?)", row.Objectcode, row.Object, row.Categorie, row.Type, row.Unnamed, row.Afmeting, row.Beschrijving, row.Operationeel, row.Conditie, row.Foto)
+cnxn.commit()
+cursor.close()
 
 # for item in range(0, 44):
 #     # print(df[item])
